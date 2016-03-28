fcc4b8acad04b2b26e78c6b9ece21456c9875385
Paige Bolduc <paigebolduc@gmail.com>
2016-03-28 13:30:21 -0500

    add nested placeholder views

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var app = angular.module('bristlecone', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', { url: '/', templateUrl: './partials/home.html' })  // give it a name and a configuration object that contains a url, a template
+    .state('home', {
+      url: '/',
+      views: {
+        "main": {templateUrl: './partials/home.html' },
+        "innerViews": {templateUrl: './partials/inner2.html' },
+      }
+    })  // give it a name and a configuration object that contains a url, a template
+
     .state('view1', { url: '/view1', templateUrl: './partials/view1.html'})  // give it a name and a configuration object that contains a url, a template
     .state('view2', { url: '/view2', templateUrl: './partials/view2.html'})  // give it a name and a configuration object that contains a url, a template
     .state('view3', { url: '/view3', templateUrl: './partials/view3.html'})  // give it a name and a configuration object that contains a url, a template
+    .state('view1.inner', { url: '/inner', templateUrl: './partials/inner.html'})  // give it a name and a configuration object that contains a url, a template
+    .state('view1.inner2', { url: '/inner2', templateUrl: './partials/inner2.html'})  // give it a name and a configuration object that contains a url, a template
+    .state('view1.inner3', { url: '/inner3', templateUrl: './partials/inner3.html'})  // give it a name and a configuration object that contains a url, a template
 
   $urlRouterProvider.otherwise('/'); // gives a default view
 });
